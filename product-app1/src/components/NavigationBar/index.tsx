b12b2fa5678ce96aee940dd611239a70d71b4bf1
Javinator11 <sosa.puga.javier@gmail.com>
2021-09-25 10:06:27 -0600

    First Commit

---
+++ product-app1/src/components/NavigationBar/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import "./index.css";
+import { AppBar, IconButton, Typography, Toolbar } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+/**
+ * Navigation bar elements
+ * @returns NavigationBar UI elements
+ */
+const NavigationBar = () => {
+    return(
+        <AppBar position="static" className="navbar">
+        <Toolbar>
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            ChambaSoft Store
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+    );
+};
+
+export default NavigationBar;
---
+++ product-app1/src/types/Product.ts
@@ -0,0 +1,9 @@
+import Sku from "./Sku";
+
+
+export default interface Product {    
+    id?: number,    
+    name: string,    
+    description: string,    
+    childSkus: [Sku]
+}
